baffc416f159ca4d81f99ee0beab87eb8afec8ac
JonVer403 <jcverhoeven@outlook.com>
2026-03-11 13:44:33 +0100

    php files toegevoegd

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <section class="inlogSect">
+            <h1>inloggen</h1>
+            <form action="login.php" method="post">
+                <label for="email">email</label>
+                <input type="email" name="email" id="email" required>
+                <label for="password">wachtwoord</label>
+                <input type="password" name="password" id="password" required>
+                <input type="submit" value="inloggen">
+            </form>
+        </section>
+    </main>
+</body>
+</html>
