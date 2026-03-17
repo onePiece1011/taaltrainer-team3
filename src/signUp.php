778a92160274cd0898e9b4de855e6ab83aa014d7
onePiece1011 <hannanismail08@gmail.com>
2026-03-17 11:08:32 +0100

    commit

--- src/signUp.php
+++ src/signUp.php
@@ -1,3 +1,36 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "taaltrainer_t3";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        // Check if username already exists
+        $check_sql = "SELECT * FROM users WHERE username = '$username'";
+        $check_result = $conn->query($check_sql);
+
+        if($check_result->num_rows > 0){
+            echo "Username already exists";
+        } else {
+            $sql = "INSERT INTO users (username, email, PASSWORD) VALUES ('$username', NULL, '$password')";
+            if($conn->query($sql) === TRUE){
+                header("Location: login.php");
+                exit();
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,19 +42,19 @@
 <body>
     <main>
         <section class="inlogSect">
-            <div>  
+            <div>
                 <h1>Sign up</h1>
-                <form action="login.php" method="post">
+                <form action="signUp.php" method="post">
                     <label for="username">username</label>
-                    <input type="username" name="username" id="username" required>
+                    <input type="text" name="username" id="username" required>
                     <label for="password">wachtwoord</label>
                     <input type="password" name="password" id="password" required>
-                    <input type="submit" value="inloggen">
+                    <input type="submit" value="Sign up" name="submit">
                 </form>
             </div>
         </section>
         <div>
-            <p>Heb je al een account? <button><a href="login.php">Log hier in</a></button></p>
+            <p>Heb je al een account? <a href="login.php"><button>Log hier in</button></a></p>
         </div>
     </main>
 </body>
