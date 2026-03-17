88e9d3102abca870e4cb866b734aa2b80fa2a540
JonVer403 <jcverhoeven@outlook.com>
2026-03-17 10:04:27 +0100

    indexindexpiemel

--- src/login.php
+++ src/login.php
@@ -11,12 +11,12 @@
         <section class="inlogSect">
             <div style="text-align:center" class=>  
                 <h1>inloggen</h1>
-                <form action="login.php" method="post">
+                <form action="index.php" method="post">
                     <label for="username">username</label>
-                    <input type="username" name="username" id="username" required>
+                    <input type="text" name="username" id="username" required><br><br>
                     <label for="password">wachtwoord</label>
-                    <input type="password" name="password" id="password" required>
-                    <input type="submit" value="inloggen">
+                    <input type="password" name="password" id="password" required><br><br>
+                    <input type="submit" id="btn" value="Login" name="submit">
                 </form>
             </div>
         </section>
