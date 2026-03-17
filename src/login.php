a867d8c65d254dbdf977696f501ad20768d59c34
JonVer403 <jcverhoeven@outlook.com>
2026-03-17 11:01:46 +0100

    login test

--- src/login.php
+++ src/login.php
@@ -1,5 +1,15 @@
 <?php
-    include ('connection.php');
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "taaltrainer";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -14,7 +24,7 @@
         <section class="inlogSect">
             <div style="text-align:center" class=>  
                 <h1>inloggen</h1>
-                <form action="index.php" method="post">
+                <form action="login.php" method="post">
                     <label for="username">username</label>
                     <input type="text" name="username" id="username" required><br><br>
                     <label for="password">wachtwoord</label>
@@ -27,6 +37,22 @@
             <p>Heb je nog geen account? <a href="signUp.php"><button>Maak hier aan</button></a></p>
         </div>
     </main>
+    <?php
+        if(isset($_POST['submit'])){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+            $result = $conn->query($sql);
+
+            if($result->num_rows > 0){
+                header("Location: index.php");
+                echo "Login successful";
+            } else {
+               echo "Login failed";
+            }
+    }
+    ?>
     <a href="index.php">test knop verwijderen</a>
 </body>
 </html>
