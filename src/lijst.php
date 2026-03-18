4f7c6bc2aef3583d6c549ac1d6711c5199b8a4c6
onePiece1011 <hannanismail08@gmail.com>
2026-03-18 14:07:33 +0100

    lijst table

--- src/lijst.php
+++ src/lijst.php
@@ -13,129 +13,174 @@
     <main>
         <section>
             <h2>Eten & Drinken</h2>
-            <ul>
-                <li>Water<br>su<br>Av</li>
-                <li>Brood<br>Ekmek<br>Nan</li>
-                <li>Melk<br>Süt<br>Şîr</li>
-                <li>Appel<br>Elma<br>Sêv</li>
-                <li>Ei<br>Yumurta<br>Hêk</li>
-                <li>Kaas<br>Peynir<br>Penêr</li>
-                <li>Vlees<br>Et<br>Goşt</li>
-                <li>Rijst<br>Pirinç<br>Birinc</li>
-                <li>Thee<br>Çay<br>Çay</li>
-                <li>Koffie<br>Kahve<br>Qehve</li>
-                <li>Suiker<br>Şeker<br>Şekir</li>
-                <li>Zout<br>Tuz<br>Xwê</li>
-                <li>Groente<br>Sebze<br>Keskahî</li>
-                <li>Fruit<br>Meyve<br>Fêkî</li>
-                <li>Soep<br>Çorba<br>Şorbe</li>
-                <li>Boter<br>Tereyağı<br>Rûnê nivîşk</li>
-                <li>Honing<br>Bal<br>Hingiv</li>
-                <li>Vis<br>Balık<br>Masî</li>
-                <li>Kip<br>Tavuk<br>Mirîşk</li>
-                <li>Ui<br>Soğan<br>Pîvaz</li>
-                <li>Knoflook<br>Sarımsak<br>Sîr</li>
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nederlands</th>
+                        <th>Turks</th>
+                        <th>Koerdisch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Water</td><td>su</td><td>Av</td></tr>
+                    <tr><td>Brood</td><td>Ekmek</td><td>Nan</td></tr>
+                    <tr><td>Melk</td><td>Süt</td><td>Şîr</td></tr>
+                    <tr><td>Appel</td><td>Elma</td><td>Sêv</td></tr>
+                    <tr><td>Ei</td><td>Yumurta</td><td>Hêk</td></tr>
+                    <tr><td>Kaas</td><td>Peynir</td><td>Penêr</td></tr>
+                    <tr><td>Vlees</td><td>Et</td><td>Goşt</td></tr>
+                    <tr><td>Rijst</td><td>Pirinç</td><td>Birinc</td></tr>
+                    <tr><td>Thee</td><td>Çay</td><td>Çay</td></tr>
+                    <tr><td>Koffie</td><td>Kahve</td><td>Qehve</td></tr>
+                    <tr><td>Suiker</td><td>Şeker</td><td>Şekir</td></tr>
+                    <tr><td>Zout</td><td>Tuz</td><td>Xwê</td></tr>
+                    <tr><td>Groente</td><td>Sebze</td><td>Keskahî</td></tr>
+                    <tr><td>Fruit</td><td>Meyve</td><td>Fêkî</td></tr>
+                    <tr><td>Soep</td><td>Çorba</td><td>Şorbe</td></tr>
+                    <tr><td>Boter</td><td>Tereyağı</td><td>Rûnê nivîşk</td></tr>
+                    <tr><td>Honing</td><td>Bal</td><td>Hingiv</td></tr>
+                    <tr><td>Vis</td><td>Balık</td><td>Masî</td></tr>
+                    <tr><td>Kip</td><td>Tavuk</td><td>Mirîşk</td></tr>
+                    <tr><td>Ui</td><td>Soğan</td><td>Pîvaz</td></tr>
+                    <tr><td>Knoflook</td><td>Sarımsak</td><td>Sîr</td></tr>
+                </tbody>
+            </table>
         </section>
         <section>
             <h2>Familie</h2>
-            <ul>
-                <li>Moeder<br>Anne<br>Dayik</li>
-                <li>Vader<br>Baba<br>Bav</li>
-                <li>Zoon<br>Oğul<br>Kur</li>
-                <li>Dochter<br>Kız evlat<br>Keç</li>
-                <li>Broer<br>Erkek kardeş<br>Biray</li>
-                <li>Zus<br>Kız kardeş<br>Xwişk</li>
-                <li>Oma<br>Büyükanne<br>Dapîr</li>
-                <li>Opa<br>Büyükbaba<br>Bapîr</li>
-                <li>Kind<br>Çocuk<br>Zarok</li>
-                <li>Baby<br>Bebek<br>Sava</li>
-                <li>Oom (vaderskant)<br>Amca<br>Mam</li>
-                <li>Oom (moederskant)<br>Dayı<br>Xal</li>
-                <li>Tante (vaderskant)<br>Hala<br>Met</li>
-                <li>Tante (moederskant)<br>Teyze<br>Xaltî</li>
-                <li>Neef<br>Yeğen/Kuzen<br>Kurê ap/met/xal/xaltî</li>
-                <li>Nicht<br>Yeğen/Kuzen<br>Keça ap/met/xal/xaltî</li>
-                <li>Vrouw/Echtgenote<br>Eş/Kadın<br>Jin</li>
-                <li>Man/Echtgenoot<br>Eş/Adam<br>Mêr</li>
-                <li>Vriend<br>Arkadaş<br>Heval</li>
-                <li>Familie<br>Aile<br>Malbat</li>
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nederlands</th>
+                        <th>Turks</th>
+                        <th>Koerdisch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Moeder</td><td>Anne</td><td>Dayik</td></tr>
+                    <tr><td>Vader</td><td>Baba</td><td>Bav</td></tr>
+                    <tr><td>Zoon</td><td>Oğul</td><td>Kur</td></tr>
+                    <tr><td>Dochter</td><td>Kız evlat</td><td>Keç</td></tr>
+                    <tr><td>Broer</td><td>Erkek kardeş</td><td>Biray</td></tr>
+                    <tr><td>Zus</td><td>Kız kardeş</td><td>Xwişk</td></tr>
+                    <tr><td>Oma</td><td>Büyükanne</td><td>Dapîr</td></tr>
+                    <tr><td>Opa</td><td>Büyükbaba</td><td>Bapîr</td></tr>
+                    <tr><td>Kind</td><td>Çocuk</td><td>Zarok</td></tr>
+                    <tr><td>Baby</td><td>Bebek</td><td>Sava</td></tr>
+                    <tr><td>Oom (vaderskant)</td><td>Amca</td><td>Mam</td></tr>
+                    <tr><td>Oom (moederskant)</td><td>Dayı</td><td>Xal</td></tr>
+                    <tr><td>Tante (vaderskant)</td><td>Hala</td><td>Met</td></tr>
+                    <tr><td>Tante (moederskant)</td><td>Teyze</td><td>Xaltî</td></tr>
+                    <tr><td>Neef</td><td>Yeğen/Kuzen</td><td>Kurê ap/met/xal/xaltî</td></tr>
+                    <tr><td>Nicht</td><td>Yeğen/Kuzen</td><td>Keça ap/met/xal/xaltî</td></tr>
+                    <tr><td>Vrouw/Echtgenote</td><td>Eş/Kadın</td><td>Jin</td></tr>
+                    <tr><td>Man/Echtgenoot</td><td>Eş/Adam</td><td>Mêr</td></tr>
+                    <tr><td>Vriend</td><td>Arkadaş</td><td>Heval</td></tr>
+                    <tr><td>Familie</td><td>Aile</td><td>Malbat</td></tr>
+                </tbody>
+            </table>
         </section>
         <section>
             <h2>Getallen</h2>
-            <ul>
-                <li>Eén<br>Bir<br>Yek</li>
-                <li>Twee<br>İki<br>Du</li>
-                <li>Drie<br>Üç<br>Sê</li>
-                <li>Vier<br>Dört<br>Çar</li>
-                <li>Vijf<br>Beş<br>Pênc</li>
-                <li>Zes<br>Altı<br>Şeş</li>
-                <li>Zeven<br>Yedi<br>Heft</li>
-                <li>Acht<br>Sekiz<br>Heşt</li>
-                <li>Negen<br>Dokuz<br>Neh</li>
-                <li>Tien<br>On<br>Deh</li>
-                <li>Elf<br>On bir<br>Yazdeh</li>
-                <li>Twaalf<br>On iki<br>Duwazdeh</li>
-                <li>Twintig<br>Yirmi<br>Bîst</li>
-                <li>Dertig<br>Otuz<br>Sî</li>
-                <li>Veertig<br>Kırk<br>Çil</li>
-                <li>Vijftig<br>Elli<br>Pêncî</li>
-                <li>Honderd<br>Yüz<br>Sed</li>
-                <li>Duizend<br>Bin<br>Hezar</li>
-                <li>Eerste<br>Birinci<br>Yekem</li>
-                <li>Tweede<br>İkinci<br>Duwem</li>
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nederlands</th>
+                        <th>Turks</th>
+                        <th>Koerdisch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Eén</td><td>Bir</td><td>Yek</td></tr>
+                    <tr><td>Twee</td><td>İki</td><td>Du</td></tr>
+                    <tr><td>Drie</td><td>Üç</td><td>Sê</td></tr>
+                    <tr><td>Vier</td><td>Dört</td><td>Çar</td></tr>
+                    <tr><td>Vijf</td><td>Beş</td><td>Pênc</td></tr>
+                    <tr><td>Zes</td><td>Altı</td><td>Şeş</td></tr>
+                    <tr><td>Zeven</td><td>Yedi</td><td>Heft</td></tr>
+                    <tr><td>Acht</td><td>Sekiz</td><td>Heşt</td></tr>
+                    <tr><td>Negen</td><td>Dokuz</td><td>Neh</td></tr>
+                    <tr><td>Tien</td><td>On</td><td>Deh</td></tr>
+                    <tr><td>Elf</td><td>On bir</td><td>Yazdeh</td></tr>
+                    <tr><td>Twaalf</td><td>On iki</td><td>Duwazdeh</td></tr>
+                    <tr><td>Twintig</td><td>Yirmi</td><td>Bîst</td></tr>
+                    <tr><td>Dertig</td><td>Otuz</td><td>Sî</td></tr>
+                    <tr><td>Veertig</td><td>Kırk</td><td>Çil</td></tr>
+                    <tr><td>Vijftig</td><td>Elli</td><td>Pêncî</td></tr>
+                    <tr><td>Honderd</td><td>Yüz</td><td>Sed</td></tr>
+                    <tr><td>Duizend</td><td>Bin</td><td>Hezar</td></tr>
+                    <tr><td>Eerste</td><td>Birinci</td><td>Yekem</td></tr>
+                    <tr><td>Tweede</td><td>İkinci</td><td>Duwem</td></tr>
+                </tbody>
+            </table>
         </section>
         <section>
             <h2>Tijd & Dagen</h2>
-            <ul>
-                <li>Maandag<br>Pazartesi<br>Duşem</li>
-                <li>Dinsdag<br>Salı<br>Sêşem</li>
-                <li>Woensdag<br>Çarşamba<br>Çarşem</li>
-                <li>Donderdag<br>Perşembe<br>Pêncşem</li>
-                <li>Vrijdag<br>Cuma<br>În</li>
-                <li>Zaterdag<br>Cumartesi<br>Şemî</li>
-                <li>Zondag<br>Pazar<br>Yekşem</li>
-                <li>Vandaag<br>Bugün<br>Îro</li>
-                <li>Gisteren<br>Dün<br>Duh</li>
-                <li>Morgen<br>Yarın<br>Sibê</li>
-                <li>Dag<br>Gün<br>Roj</li>
-                <li>Nacht<br>Gece<br>Şev</li>
-                <li>Ochtend<br>Sabah<br>Sibe</li>
-                <li>Avond<br>Akşam<br>Êvar</li>
-                <li>Week<br>Hafta<br>Hevte</li>
-                <li>Maand<br>Ay<br>Meh</li>
-                <li>Jaar<br>Yıl<br>Sal</li>
-                <li>Uur<br>Saat<br>Saet</li>
-                <li>Minuut<br>Dakika<br>Xulek</li>
-                <li>Nu<br>Şimdi<br>Niha</li>
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nederlands</th>
+                        <th>Turks</th>
+                        <th>Koerdisch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Maandag</td><td>Pazartesi</td><td>Duşem</td></tr>
+                    <tr><td>Dinsdag</td><td>Salı</td><td>Sêşem</td></tr>
+                    <tr><td>Woensdag</td><td>Çarşamba</td><td>Çarşem</td></tr>
+                    <tr><td>Donderdag</td><td>Perşembe</td><td>Pêncşem</td></tr>
+                    <tr><td>Vrijdag</td><td>Cuma</td><td>În</td></tr>
+                    <tr><td>Zaterdag</td><td>Cumartesi</td><td>Şemî</td></tr>
+                    <tr><td>Zondag</td><td>Pazar</td><td>Yekşem</td></tr>
+                    <tr><td>Vandaag</td><td>Bugün</td><td>Îro</td></tr>
+                    <tr><td>Gisteren</td><td>Dün</td><td>Duh</td></tr>
+                    <tr><td>Morgen</td><td>Yarın</td><td>Sibê</td></tr>
+                    <tr><td>Dag</td><td>Gün</td><td>Roj</td></tr>
+                    <tr><td>Nacht</td><td>Gece</td><td>Şev</td></tr>
+                    <tr><td>Ochtend</td><td>Sabah</td><td>Sibe</td></tr>
+                    <tr><td>Avond</td><td>Akşam</td><td>Êvar</td></tr>
+                    <tr><td>Week</td><td>Hafta</td><td>Hevte</td></tr>
+                    <tr><td>Maand</td><td>Ay</td><td>Meh</td></tr>
+                    <tr><td>Jaar</td><td>Yıl</td><td>Sal</td></tr>
+                    <tr><td>Uur</td><td>Saat</td><td>Saet</td></tr>
+                    <tr><td>Minuut</td><td>Dakika</td><td>Xulek</td></tr>
+                    <tr><td>Nu</td><td>Şimdi</td><td>Niha</td></tr>
+                </tbody>
+            </table>
         </section>
         <section>
             <h2>Dagelijkse Voorwerpen</h2>
-            <ul>
-                <li>Huis<br>Ev<br>Mal</li>
-                <li>Auto<br>Araba<br>Erebî</li>
-                <li>Boek<br>Kitap<br>Pirtûk</li>
-                <li>Pen<br>Kalem<br>Pênûs</li>
-                <li>Telefoon<br>Telefon<br>Telefon</li>
-                <li>Sleutel<br>Anahtar<br>Mifte</li>
-                <li>Tafel<br>Masa<br>Mase</li>
-                <li>Stoel<br>Sandalye<br>Kursî</li>
-                <li>Deur<br>Kapı<br>Derî</li>
-                <li>Raam<br>Pencere<br>Pencere</li>
-                <li>Bed<br>Yatak<br>Cî/Nivîn</li>
-                <li>Kleding<br>Kıyafet<br>Cil</li>
-                <li>Schoenen<br>Ayakkabı<br>Pêlav</li>
-                <li>Tas<br>Çanta<br>Çente</li>
-                <li>Geld<br>Para<br>Pere</li>
-                <li>Krant<br>Gazete<br>Rojname</li>
-                <li>School<br>Okul<br>Dibistan</li>
-                <li>Stad<br>Şehir<br>Bajar</li>
-                <li>Weg<br>Yol<br>Rê</li>
-                <li>Licht<br>Işık<br>Ronahî</li>
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Nederlands</th>
+                        <th>Turks</th>
+                        <th>Koerdisch</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Huis</td><td>Ev</td><td>Mal</td></tr>
+                    <tr><td>Auto</td><td>Araba</td><td>Erebî</td></tr>
+                    <tr><td>Boek</td><td>Kitap</td><td>Pirtûk</td></tr>
+                    <tr><td>Pen</td><td>Kalem</td><td>Pênûs</td></tr>
+                    <tr><td>Telefoon</td><td>Telefon</td><td>Telefon</td></tr>
+                    <tr><td>Sleutel</td><td>Anahtar</td><td>Mifte</td></tr>
+                    <tr><td>Tafel</td><td>Masa</td><td>Mase</td></tr>
+                    <tr><td>Stoel</td><td>Sandalye</td><td>Kursî</td></tr>
+                    <tr><td>Deur</td><td>Kapı</td><td>Derî</td></tr>
+                    <tr><td>Raam</td><td>Pencere</td><td>Pencere</td></tr>
+                    <tr><td>Bed</td><td>Yatak</td><td>Cî/Nivîn</td></tr>
+                    <tr><td>Kleding</td><td>Kıyafet</td><td>Cil</td></tr>
+                    <tr><td>Schoenen</td><td>Ayakkabı</td><td>Pêlav</td></tr>
+                    <tr><td>Tas</td><td>Çanta</td><td>Çente</td></tr>
+                    <tr><td>Geld</td><td>Para</td><td>Pere</td></tr>
+                    <tr><td>Krant</td><td>Gazete</td><td>Rojname</td></tr>
+                    <tr><td>School</td><td>Okul</td><td>Dibistan</td></tr>
+                    <tr><td>Stad</td><td>Şehir</td><td>Bajar</td></tr>
+                    <tr><td>Weg</td><td>Yol</td><td>Rê</td></tr>
+                    <tr><td>Licht</td><td>Işık</td><td>Ronahî</td></tr>
+                </tbody>
+            </table>
         </section>
     </main>
 </body>
