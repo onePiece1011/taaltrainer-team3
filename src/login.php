3a74f9ad43ffeaeee5be9d46fb5361144d3fa793
JonVer403 <jcverhoeven@outlook.com>
2026-03-18 14:17:09 +0100

    database cnnect

--- src/login.php
+++ src/login.php
@@ -1,15 +1,5 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "taaltrainer";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    echo "";
+    include 'DatabaseConnect.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- src/signUp.php
+++ src/signUp.php
@@ -1,14 +1,5 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "taaltrainer";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    include 'DatabaseConnect.php';
 ?>
     
 <!DOCTYPE html>
