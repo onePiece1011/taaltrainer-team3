170baf45bdaa0c9c5de9fca957a919f820a75e57
onePiece1011 <hannanismail08@gmail.com>
2026-03-18 14:02:15 +0100

    lijst van woorden

---
+++ src/lijst.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woordenlijst - Taaltrainer</title>
+    <link rel="stylesheet" href="css/startpagina.css">
+</head>
+<body>
+    <header>
+        <h1>Woordenlijst</h1>
+    </header>
+    <main>
+        <section>
+            <h2>Eten & Drinken</h2>
+            <ul>
+                <li>Water<br>su<br>Av</li>
+                <li>Brood<br>Ekmek<br>Nan</li>
+                <li>Melk<br>Süt<br>Şîr</li>
+                <li>Appel<br>Elma<br>Sêv</li>
+                <li>Ei<br>Yumurta<br>Hêk</li>
+                <li>Kaas<br>Peynir<br>Penêr</li>
+                <li>Vlees<br>Et<br>Goşt</li>
+                <li>Rijst<br>Pirinç<br>Birinc</li>
+                <li>Thee<br>Çay<br>Çay</li>
+                <li>Koffie<br>Kahve<br>Qehve</li>
+                <li>Suiker<br>Şeker<br>Şekir</li>
+                <li>Zout<br>Tuz<br>Xwê</li>
+                <li>Groente<br>Sebze<br>Keskahî</li>
+                <li>Fruit<br>Meyve<br>Fêkî</li>
+                <li>Soep<br>Çorba<br>Şorbe</li>
+                <li>Boter<br>Tereyağı<br>Rûnê nivîşk</li>
+                <li>Honing<br>Bal<br>Hingiv</li>
+                <li>Vis<br>Balık<br>Masî</li>
+                <li>Kip<br>Tavuk<br>Mirîşk</li>
+                <li>Ui<br>Soğan<br>Pîvaz</li>
+                <li>Knoflook<br>Sarımsak<br>Sîr</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Familie</h2>
+            <ul>
+                <li>Moeder<br>Anne<br>Dayik</li>
+                <li>Vader<br>Baba<br>Bav</li>
+                <li>Zoon<br>Oğul<br>Kur</li>
+                <li>Dochter<br>Kız evlat<br>Keç</li>
+                <li>Broer<br>Erkek kardeş<br>Biray</li>
+                <li>Zus<br>Kız kardeş<br>Xwişk</li>
+                <li>Oma<br>Büyükanne<br>Dapîr</li>
+                <li>Opa<br>Büyükbaba<br>Bapîr</li>
+                <li>Kind<br>Çocuk<br>Zarok</li>
+                <li>Baby<br>Bebek<br>Sava</li>
+                <li>Oom (vaderskant)<br>Amca<br>Mam</li>
+                <li>Oom (moederskant)<br>Dayı<br>Xal</li>
+                <li>Tante (vaderskant)<br>Hala<br>Met</li>
+                <li>Tante (moederskant)<br>Teyze<br>Xaltî</li>
+                <li>Neef<br>Yeğen/Kuzen<br>Kurê ap/met/xal/xaltî</li>
+                <li>Nicht<br>Yeğen/Kuzen<br>Keça ap/met/xal/xaltî</li>
+                <li>Vrouw/Echtgenote<br>Eş/Kadın<br>Jin</li>
+                <li>Man/Echtgenoot<br>Eş/Adam<br>Mêr</li>
+                <li>Vriend<br>Arkadaş<br>Heval</li>
+                <li>Familie<br>Aile<br>Malbat</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Getallen</h2>
+            <ul>
+                <li>Eén<br>Bir<br>Yek</li>
+                <li>Twee<br>İki<br>Du</li>
+                <li>Drie<br>Üç<br>Sê</li>
+                <li>Vier<br>Dört<br>Çar</li>
+                <li>Vijf<br>Beş<br>Pênc</li>
+                <li>Zes<br>Altı<br>Şeş</li>
+                <li>Zeven<br>Yedi<br>Heft</li>
+                <li>Acht<br>Sekiz<br>Heşt</li>
+                <li>Negen<br>Dokuz<br>Neh</li>
+                <li>Tien<br>On<br>Deh</li>
+                <li>Elf<br>On bir<br>Yazdeh</li>
+                <li>Twaalf<br>On iki<br>Duwazdeh</li>
+                <li>Twintig<br>Yirmi<br>Bîst</li>
+                <li>Dertig<br>Otuz<br>Sî</li>
+                <li>Veertig<br>Kırk<br>Çil</li>
+                <li>Vijftig<br>Elli<br>Pêncî</li>
+                <li>Honderd<br>Yüz<br>Sed</li>
+                <li>Duizend<br>Bin<br>Hezar</li>
+                <li>Eerste<br>Birinci<br>Yekem</li>
+                <li>Tweede<br>İkinci<br>Duwem</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Tijd & Dagen</h2>
+            <ul>
+                <li>Maandag<br>Pazartesi<br>Duşem</li>
+                <li>Dinsdag<br>Salı<br>Sêşem</li>
+                <li>Woensdag<br>Çarşamba<br>Çarşem</li>
+                <li>Donderdag<br>Perşembe<br>Pêncşem</li>
+                <li>Vrijdag<br>Cuma<br>În</li>
+                <li>Zaterdag<br>Cumartesi<br>Şemî</li>
+                <li>Zondag<br>Pazar<br>Yekşem</li>
+                <li>Vandaag<br>Bugün<br>Îro</li>
+                <li>Gisteren<br>Dün<br>Duh</li>
+                <li>Morgen<br>Yarın<br>Sibê</li>
+                <li>Dag<br>Gün<br>Roj</li>
+                <li>Nacht<br>Gece<br>Şev</li>
+                <li>Ochtend<br>Sabah<br>Sibe</li>
+                <li>Avond<br>Akşam<br>Êvar</li>
+                <li>Week<br>Hafta<br>Hevte</li>
+                <li>Maand<br>Ay<br>Meh</li>
+                <li>Jaar<br>Yıl<br>Sal</li>
+                <li>Uur<br>Saat<br>Saet</li>
+                <li>Minuut<br>Dakika<br>Xulek</li>
+                <li>Nu<br>Şimdi<br>Niha</li>
+            </ul>
+        </section>
+        <section>
+            <h2>Dagelijkse Voorwerpen</h2>
+            <ul>
+                <li>Huis<br>Ev<br>Mal</li>
+                <li>Auto<br>Araba<br>Erebî</li>
+                <li>Boek<br>Kitap<br>Pirtûk</li>
+                <li>Pen<br>Kalem<br>Pênûs</li>
+                <li>Telefoon<br>Telefon<br>Telefon</li>
+                <li>Sleutel<br>Anahtar<br>Mifte</li>
+                <li>Tafel<br>Masa<br>Mase</li>
+                <li>Stoel<br>Sandalye<br>Kursî</li>
+                <li>Deur<br>Kapı<br>Derî</li>
+                <li>Raam<br>Pencere<br>Pencere</li>
+                <li>Bed<br>Yatak<br>Cî/Nivîn</li>
+                <li>Kleding<br>Kıyafet<br>Cil</li>
+                <li>Schoenen<br>Ayakkabı<br>Pêlav</li>
+                <li>Tas<br>Çanta<br>Çente</li>
+                <li>Geld<br>Para<br>Pere</li>
+                <li>Krant<br>Gazete<br>Rojname</li>
+                <li>School<br>Okul<br>Dibistan</li>
+                <li>Stad<br>Şehir<br>Bajar</li>
+                <li>Weg<br>Yol<br>Rê</li>
+                <li>Licht<br>Işık<br>Ronahî</li>
+            </ul>
+        </section>
+    </main>
+</body>
+</html>
