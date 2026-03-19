7f22335510f182225e85b0da5ab8abcb174e4f71
JonVer403 <jcverhoeven@outlook.com>
2026-03-19 10:00:35 +0100

    afds

--- src/turkswoordenleren.php
+++ src/turkswoordenleren.php
@@ -114,6 +114,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Turks Leren</title>
+    <link rel="stylesheet" href="css/1vooraalles.css">
 </head>
 <body>
     <main>
