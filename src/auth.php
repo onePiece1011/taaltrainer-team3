746f065039f8b78a6a76634e33153591470d3408
JonVer403 <jcverhoeven@outlook.com>
2026-03-23 09:24:48 +0100

    enter

--- src/auth.php
+++ src/auth.php
@@ -1,7 +1,4 @@
 <?php
-// Enforce login on pages that include this file.
-// Include this at the very top of the page (before any HTML output).
-
 session_start();
 
 if (!isset($_SESSION['username'])) {
