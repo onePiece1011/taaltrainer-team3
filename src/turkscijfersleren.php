93a319c7f07d585efd4ccfc684433d84499b6259
JonVer403 <jcverhoeven@outlook.com>
2026-03-24 10:12:12 +0100

    test streaks

--- src/turkscijfersleren.php
+++ src/turkscijfersleren.php
@@ -135,9 +135,9 @@
                         $yesterday = date('Y-m-d', strtotime("-1 day"));
 
                         // 1. Haal de huidige streak gegevens op
-                        $sql = "SELECT streak, last_date FROM users WHERE username = '$username'";
-                        $streakData = $conn->query($sql);
-                        
+                        $query = $db->prepare("SELECT streak, last_date FROM users WHERE username = '$username'");
+                        $query->execute();
+                        $streakData = $query->fetch();
 
                         if (!$streakData) {
                            // Gebruiker heeft nog nooit een streak gehad
